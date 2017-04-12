455db10a8dd1ff85dffef52d66b35e4143629090
Tearar <mps.adlhoch@gmail.com>
2017-04-12 19:41:41 +0200

    commented GameWindow.cs

--- LunarLander_picture/GameConfig.cs
+++ LunarLander_picture/GameConfig.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LunarLander_picture
 {
@@ -14,13 +10,23 @@ namespace LunarLander_picture
         public const String FONT = "Stencil";
         public const String WELCOME = "Welcome to Lunar Lander !";
         public const String DIFFICULTY = "Choose the difficulty:";
+        public const String GAME_OVER = "Game over !";
+        public const String RESTART = "Press Enter to restart the game !";
 
         public const int ZERO = 0;
         public const int WELCOME_FONTSIZE = 30;
+        public const int RESTART_FONTSIZE = 20;
         public const int WELCOME_X = 175;
         public const int WELCOME_Y = 50;
         public const int DIFFICULTY_X = 200;
         public const int DIFFICULTY_Y = 100;
+        public const int STARTING_FUEL = 100;
+        public const int TIMER_TICK = 20;
+        public const int GAME_OVER_X = 275;
+        public const int GAME_OVER_Y = 375;
+        public const int RESTART_X = 150;
+        public const int RESTART_Y = 450;
+
 
 
 
--- LunarLander_picture/GameWindow.cs
+++ LunarLander_picture/GameWindow.cs
@@ -8,157 +8,167 @@ namespace LunarLander_picture
 {
     public partial class GameWindow : Form
     {
-        //Background _background;
-        
+        /* initialize class variables*/
         Game _game;
-        int difficulty;
         Graphics gfx;
-        public bool test = true;
-        bool run = true;
+        SoundPlayer engines = new SoundPlayer(Properties.Resources.engines);
+        int difficulty;
         int landerState = (int)LanderState.Off;
         int gameState = (int)GameState.Running;
-        int fuel = 100;
-        SoundPlayer engines = new SoundPlayer(Properties.Resources.engines);
-   
+        int fuel = GameConfig.STARTING_FUEL;
 
-
-
-
-
-
-
-
+        /* public constructor */
         public GameWindow(int difficulty)
         {
-            this.KeyPreview = true;
+            InitializeComponent(); 
             this.difficulty = difficulty;
-            InitializeComponent();
+            KeyPreview = true;
             DoubleBuffered = true;
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             gfx = Graphics.FromImage(pictureBox1.Image);
             _game = new Game(gfx, difficulty);
-          
-            timer1.Interval = 20;
-            timer1.Start();   
+            handleTimer();         
         }
 
-       
+        /* sets the timer interval and starts it. */
+        private void handleTimer()
+        {
+            timer1.Interval = GameConfig.TIMER_TICK;
+            timer1.Start();
+        }
 
         void pictureBox1_KeyPress(object sender, KeyPressEventArgs e){ }
 
+        /* handles the game in a tick event. Actions depend on the gameState variable. */
         private void timer1_Tick(object sender, EventArgs e)
         {
-         
            switch(gameState)
             {
                 case (int)GameState.Running:
-                    _game.Move();
-                    _game.MoveLander(landerState);
-                    _game.handleHUD(fuel);
-                   
-                    gameState = _game.checkForGameState();
-                    _game.Draw(Graphics.FromImage(pictureBox1.Image));
+                    handleGameObjects();
                     break;
+
                 case (int)GameState.Won:
-                    pictureBox1.Image = Properties.Resources.game_won;
+                    setGameWonBackground();
                     break;
+
                 case (int)GameState.CollidedWithPlanet:
-                    pictureBox1.Image = Properties.Resources.game_lost;
+                    setGameLostBackground();
                     break;
+
                 case (int)GameState.CollidedWithSatellite:
-                    pictureBox1.Image = Properties.Resources.explosion_resized;
-                    Font _font = new Font("Stencil", 30, FontStyle.Italic);
-                    Font _font1 = new Font("Stencil", 20, FontStyle.Italic);
-
-                    using (Graphics g = Graphics.FromImage(pictureBox1.Image))
-                    {
-                        g.DrawString("Game Over !", _font, Brushes.Black, 275, 375); // requires font, brush etc
-                    }
-
-                    using (Graphics g = Graphics.FromImage(pictureBox1.Image))
-                    {
-                        g.DrawString("Press Enter to restart the game !", _font1, Brushes.Black, 150, 450); // requires font, brush etc
-                    }
-
-
+                    setExplosionBackground();
+                    drawLabels();
                     break;
+
                 default:
                     break;
             }
-            
-            this.Refresh();
+            Refresh();
         }
 
-     
+        /* draws game instructions */
+        private void drawLabels()
+        {
+            Font _font = new Font(GameConfig.FONT, GameConfig.WELCOME_FONTSIZE, FontStyle.Italic);
+            Font _font1 = new Font(GameConfig.FONT, GameConfig.RESTART_FONTSIZE, FontStyle.Italic);
 
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.DrawString(GameConfig.GAME_OVER, _font, Brushes.Black, GameConfig.GAME_OVER_X, GameConfig.GAME_OVER_Y);
+            }
+
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.DrawString(GameConfig.RESTART, _font1, Brushes.Black, GameConfig.RESTART_X, GameConfig.RESTART_Y);
+            }
+        }
+
+        /* sets a background to the picturebox*/
+        private void setExplosionBackground()
+        {
+            pictureBox1.Image = Properties.Resources.explosion_resized;
+        }
+
+        /* sets a background to the picturebox*/
+        private void setGameLostBackground()
+        {
+            pictureBox1.Image = Properties.Resources.game_lost;
+        }
+
+        /* sets a background to the picturebox*/
+        private void setGameWonBackground()
+        {
+            pictureBox1.Image = Properties.Resources.game_won;
+        }
+
+        /* moves the non-player objects, handles HUD, reacts to player inputs, checks constantly for the game state and draws all objects to the canvas. */
+        private void handleGameObjects()
+        {
+            _game.Move();
+            _game.MoveLander(landerState);
+            _game.handleHUD(fuel);
+             gameState = _game.checkForGameState();
+            _game.Draw(Graphics.FromImage(pictureBox1.Image));
+        }
+
+        /* handles the keyboard inputs*/
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-
             if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
-            {
-               
-                if (fuel > 0 && gameState == (int)GameState.Running)
+            {              
+                if (fuel > GameConfig.ZERO && gameState == (int)GameState.Running)
                 {
                     landerState = (int)LanderState.Up;
-                    fuel--;
-                    engines.PlayLooping();
+                    reactToInput();    
                 }
             }
 
             if(e.KeyCode == Keys.Escape)
             {
                 Application.ExitThread();
-                Environment.Exit(0);
+                Environment.Exit(GameConfig.ZERO);
             }
 
             if(e.KeyCode ==  Keys.Enter && gameState != (int)GameState.Running )
             {
-
                 restartGame();
             }
 
             if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
             {
-                if (fuel != 0 && gameState == (int)GameState.Running)
+                if (fuel != GameConfig.ZERO && gameState == (int)GameState.Running)
                 {
                     landerState = (int)LanderState.Down;
-                    fuel--;
-                    engines.PlayLooping();
+                    reactToInput();
                 }
-
-            }
-
-        
-
-           
-
+            } 
         }
 
+        /* reduces the fuel and plays the engine sound. */
+        private void reactToInput()
+        {
+            fuel--;
+            engines.PlayLooping();
+        }
+
+        /* restarts the whole application. */
         private void restartGame()
         {
             Application.ExitThread();
             Application.Restart();
-            
         }
 
+        /*handles actions when keys are released. */
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up || e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
             {
                 landerState = (int)LanderState.Off;
                 engines.Stop();
-            }
-                
+            }       
         }
 
-        private void Form1Load(object sender, EventArgs e)
-        {
-          
-        }
-
-     
+        private void Form1Load(object sender, EventArgs e) { }
     }
-
-   
-
 }
